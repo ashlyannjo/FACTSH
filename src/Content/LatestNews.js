fca0d9004d23106f016bf3d8401700a2999cf25e
ashlyannjo <102203878+ashlyannjo@users.noreply.github.com>
2024-07-12 10:10:18 +0530

    Update LatestNews.js

--- src/Content/LatestNews.js
+++ src/Content/LatestNews.js
@@ -1,16 +1,22 @@
 const News = [
        {
-        "news":"Mahesh Sasikumar, and Ebin Deni Raj. \"A short survey on Interpretable techniques with Time series data.\" In 2024 IEEE International Students' Conference on Electrical, Electronics and Computer Science (SCEECS), pp. 1-10. IEEE, 2024.",
+        "news":"Congratulations to Subodh Uniyal, Maitri Savla, Tushaar Sharma, Ashly Ann Jo, and Dr. Ebin Deni Raj for winning the Best Paper Award at the International Conference on Data Science & Exploration in Artificial Intelligence 2024 for their paper titled "Transparency in Legal AI: Pioneering Explainable NLP for Enhanced Judgement Prediction.",
         "link":"",
-        "description":"Published"
+        "description":"Best paper"
        },
        {
-        "news":"Subodh Uniyal, Maitri D. Savla, Tushaar, Ashly Ann Jo, and  Ebin Deni Raj. “Transparency in Legal AI: Pioneering Explainable NLP for  Enhanced Judgement Prediction” accepted for publication in the International Conference on Data Science & Exploration in Artificial Intelligence(CODE-AI), 2024.",
+        "news":"Anish Reddy Konyala, Ashly Ann Jo, and  Ebin Deni Raj. "Integrating Explainable AI with Machine Learning for Accurate Liver Disease Diagnosis" accepted for presentation in 2nd International Conference on Artificial Intelligence: Theory and Applications(AITA 2024), and publication in the proceedings to be published in Scopus-indexed Springer Book Series "Lecture Notes in Networks and Systems"",
         "link":"",
         "description":"Accepted"
        },
        {
-        "news":"Ashly Ann Jo and Ebin Deni Raj's paper on \"Ethical Dimensions in Data-Driven Business\" is accepted for the 15th International IEEE Conference On Computing, Communication And Networking Technologies 2024.",
+        "news":"Susmera Joseph, Ashly Ann Jo and Ebin Deni Raj's paper on "Improving Time Series Forecasting Accuracy with Transformers: A Comprehensive Analysis with
+Explainability "  is accepted for the Third International Conference on Electrical, Electronics, Information and Communication Technologies (ICEEICT 2024)",
+        "link":"",
+        "description":"Accepted"
+       },
+       {
+        "news":"Ranjith R, Ashly Ann Jo and Ebin Deni Raj's paper on "Bipol-Driven Bias Analysis in Transformer Models: A Quantitative, Global, and Local Interpretability Approach for Textual Data "  is accepted for the IEEE World Conference on Applied Intelligence and Computing (AIC 2024)",
         "link":"",
         "description":"Accepted"
        }
